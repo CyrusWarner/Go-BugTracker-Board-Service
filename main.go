00e54ada0e5de1096a365b6a7c30352f2ae14715
CyrusWarner <cyruswarner27@gmail.com>
2021-12-15 10:57:12 -0600

    Refactored the GetUsersBoards method in the models package.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
@@ -9,6 +10,10 @@ import (
 
 	"github.com/gorilla/mux"
 ) // importing the db_client package
+
+type App struct {
+	DB *sql.DB
+}
 
 func main() {
 	db_client.InitializeDBConnection()
@@ -24,6 +29,17 @@ func router() {
 
 	// First initialize the Router
 	r := mux.NewRouter() // r is the router
-	r.HandleFunc("/api/board/user/{userId:[0-9]+}", models.GetUsersBoards).Methods("GET")
+	r.HandleFunc("/api/board/user/{userId:[0-9]+}", getUsersBoards).Methods("GET")
 	log.Fatal(http.ListenAndServe(":3000", r)) // if it fails it will throw an error
+}
+
+func getUsersBoards(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r) // Gets any params in the http request
+	userId := params["userId"]
+
+	userBoards, err := models.GetUsersBoards(db_client.DBClient, userId)
+	if err != nil {
+		log.Fatalln("Getting User Boards has failed")
+	}
+
 }
--- models/models.go
+++ models/models.go
@@ -1,15 +1,13 @@
 package models
 
 import (
-	"encoding/json"
+	"database/sql"
 	"log"
-	"net/http"
 
 	"github.com/CyrusWarner/Go-BugTracker-Board-Service/db_client"
-	"github.com/gorilla/mux"
 )
 
-type UserBoards struct {
+type UserBoard struct {
 	UserId         int  `json:"userId"`
 	BoardId        int  `json:"boardId"`
 	RolesId        int  `json:"rolesId"`
@@ -23,48 +21,31 @@ type Board struct {
 	Description string `json:"description"`
 }
 
-var userBoards []*UserBoards
-
-func GetUsersBoards(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r) // Gets any params in the http request
-	userId := params["userId"]
+func GetUsersBoards(db *sql.DB, userId string) (ub []UserBoard, err error) {
 	rows, err := db_client.DBClient.Query("Select * FROM UserBoard INNER JOIN Boards ON UserBoard.BoardId=Boards.BoardId WHERE UserId=@p1 AND InviteAccepted=1", userId)
 	if err != nil {
 		log.Fatalln("Unable to Retrieve User Boards", err.Error())
 	}
 
-	println(userId)
-	userBoard := UserBoards{}
+	defer rows.Close()
+
+	userBoards := []UserBoard{}
+
 	for rows.Next() {
-		var userId, boardId, rolesId int
-		var inviteAccepted bool
-		var title, description string
-		println(title)
-		err = rows.Scan(&userId,
-			&boardId,
-			&rolesId,
-			&inviteAccepted,
-			&boardId,
-			&title,
-			&description,
+		var ub UserBoard
+		err = rows.Scan(
+			&ub.UserId,
+			&ub.BoardId,
+			&ub.RolesId,
+			&ub.InviteAccepted,
+			&ub.BoardId,
+			&ub.Title,
+			&ub.Description,
 		)
 		if err != nil {
 			log.Fatalln("Row Does Not Exist", err.Error())
 		}
-		userBoard.UserId = userId // building the board object to append to the boards variable
-		userBoard.BoardId = boardId
-		userBoard.RolesId = rolesId
-		userBoard.InviteAccepted = inviteAccepted
-		userBoard.Board.BoardId = boardId
-		userBoard.Board.Title = title
-		userBoard.Board.Description = description
-		userBoards = append(userBoards, &userBoard) // append the memory address of the board to the array of Board pointer objects
+		userBoards = append(userBoards, ub) // append the memory address of the board to the array of Board pointer objects
 	}
-
-	w.Header().Set("Content-Type", "application/json") // returns the response in JSON
-
-	// json.NewEncoder(w).Encode(userBoards)
-	json.NewEncoder(w).Encode(userBoards)
-
-	// json.NewEncoder(w).Encode(&Board{}) // returns an empty Board list if no board was found
+	return userBoards, nil
 }
